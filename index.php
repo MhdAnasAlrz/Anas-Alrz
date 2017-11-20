bd81044ba2c4bde6c39fe5cb3d5eb192365bb3fa
MhdAnasAlrz <mhd.anas.alrz@hotmail.com>
2017-11-20 18:05:22 +0100

    portfolio

---
+++ index.php
@@ -0,0 +1,394 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- UTF meta for Languages -->
+	<meta charset="utf-8">
+	<!-- internet explorar -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!-- first mobile meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- description for google -->
+	<meta name="description" content="Anas Alrz CV">
+
+	<title>ANAS | CV</title>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	<!-- google font -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:900" rel="stylesheet">
+	<!-- anas's style -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<!-- Definition of HTML5 elements for internet explorar -->
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <!-- Definition of media queris amd css3 for internet explorar -->
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<!-- jquery libraries -->
+	<script
+	src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA="
+	crossorigin="anonymous"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<!-- anas's script -->
+	<style type="text/css">
+		.education{
+			padding-bottom: 30px;
+		}
+		.education h1 , .education h5 {
+			font-weight: bold;
+		}
+		.education div p{
+			padding-top: 25px;
+    		width: 70%;
+    		margin: 0 auto;
+    		font-family: 'Roboto', sans-serif;
+		}
+	</style>
+</head>
+<body>
+<!-- slide starts -->
+<header>
+	<div id="my_slide" class="carousel slide" data-ride="carousel">
+	    <div>
+			<div class="carousel-caption">
+				<h2 class="slide_font" >Hi , I'm Anas</h2>
+				<h1 class="slide_font" >I'm Programmer
+				 & 
+				 Web Designer</h1>
+			</div>
+		</div>
+	  <!-- Wrapper for slides -->
+	  <div class="carousel-inner" role="listbox">
+	    <div class="item active">
+	      <img class="slide_img" src="img/img2.jpg" alt="Certificates">
+	    </div>
+	    <div class="item">
+	      <img class="slide_img" src="img/img3.jpg" alt="class_room">
+	    </div>
+	    <div class="item">
+	      <img class="slide_img" src="img/img4.jpg" alt="presentation">
+	    </div>
+	    <div class="item">
+	      <img class="slide_img" src="img/img5.jpg" alt="graduation">
+	    </div>
+	  </div>
+	</div>
+</header>
+ <!-- /slide -->
+
+ <!-- about me -->
+ <section>
+	 <div class="about_me text-center">
+	 	<h1>About Me</h1>
+	 	<h5>SOME INFORMATION</h5>
+	 	<hr class="line">
+		 	<div class="information" >
+		 		<p>Beginner web developer with the ability to build web apps through the entire process. I am a fast learner with previous international experience as a lightning and sound technician prior to moving to web development. My passions lie in cybersecurity and mobile. I am looking for a part-time position as LAMP developer in which I can solve real world problems in a team. My strengths are I am always direct in communicating as well as proactive in looking for solutions. One area in which I could improve is my English communication and writing skills. In a team I need people to review my text for spelling mistakes.</p>
+		 	</div>
+	 </div>
+	 <div class="footer-social-icons text-center">
+	    <ul class="social-icons">
+	        <li><a target="_blank" href="https://www.facebook.com/anas.alrz.92" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
+	        <li><a target="_blank" href="https://twitter.com/AnasAlrz" class="social-icon"> <i class="fa fa-twitter"></i></a></li>
+	        <li><a target="_blank" href="https://www.youtube.com/channel/UCXS5wxls5jBr8S_uxeWfx4w" class="social-icon"> <i class="fa fa-youtube"></i></a></li>
+	        <li><a target="_blank" href="https://nl.linkedin.com/in/mohammad-anas-alrz-748541134" class="social-icon"> <i class="fa fa-linkedin"></i></a></li>
+	        <li><a target="_blank" href="https://plus.google.com/109746493332121557953" class="social-icon"> <i class="fa fa-google-plus"></i></a></li>
+	    </ul>
+	</div>
+</section>
+<!--/about me -->
+
+<!-- personal informtion -->
+<section>
+	<div class="personal_informtion text-center">
+			<h1>Information</h1>
+		 	<h5>SOME CONTACT</h5>
+		 	<hr class="line">
+		<div class="col-lg-6 col-md-6 col-sm-6 info">
+			<i class="fa fa-home fa-5x" aria-hidden="true"></i>
+			<h5>ADDRESS</h5>
+			<p>Hazendijk 121 ,Rotterdam ,3079PG</p>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6 info">
+			<i class="fa fa-envelope-o fa-5x" aria-hidden="true"></i>
+			<h5>EMAIL</h5>
+			<p>MHD.Anas.Alrz@hotmail.com</p>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6 info">
+			<i class="fa fa-phone fa-5x" aria-hidden="true"></i>
+			<h5>PHONE</h5>
+			<p>0031 6 24 84 00 47</p>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6 info">
+			<i class="fa fa-link fa-5x" aria-hidden="true"></i>
+			<h5>LINK</h5>
+			<p>www.anas.alrez.net</p>
+		</div>
+	</div>
+</section>
+<!-- /personal informtion -->
+
+<!-- skills -->
+<section>
+	<div class="skills text-center">
+		<h1>SKILLS</h1>
+		<h5>I CAN DO</h5>
+		<hr class="line">
+		<p>Front-End Development :</p>
+			<!-- HTML5 -->
+			<div class="skillbar clearfix " data-percent="85%">
+				<div class="skillbar-title" style="background: #88cd2a;"><span>HTML5</span></div>
+				<div class="skillbar-bar" style="background: #88cd2a;"></div>
+				<div class="skill-bar-percent">85%</div>
+			</div>
+			<!-- /HTML5 -->
+			<!--  CSS3 -->
+			<div class="skillbar clearfix " data-percent="75%">
+				<div class="skillbar-title" style="background: #00a8ff;"><span>CSS3</span></div>
+				<div class="skillbar-bar" style="background: #00a8ff;"></div>
+				<div class="skill-bar-percent">75%</div>
+			</div>
+			<!-- /CSS3 -->
+			<!-- jQuery  -->
+			<div class="skillbar clearfix " data-percent="55%">
+				<div class="skillbar-title" style="background: #EB6260;"><span>JQuery</span></div>
+				<div class="skillbar-bar" style="background: #EB6260;"></div>
+				<div class="skill-bar-percent">55%</div>
+			</div>
+			<!-- /jQuery  -->
+			<p>Languages :</p>
+			<!--  javascript -->
+			<div class="skillbar clearfix " data-percent="55%">
+				<div class="skillbar-title" style="background: #EB6260;"><span>JavaScript</span></div>
+				<div class="skillbar-bar" style="background: #EB6260;"></div>
+				<div class="skill-bar-percent">55%</div>
+			</div>
+			<!-- /javascript -->
+			<!-- PHP  -->
+			<div class="skillbar clearfix " data-percent="85%">
+				<div class="skillbar-title" style="background: #88cd2a;"><span>PHP</span></div>
+				<div class="skillbar-bar" style="background: #88cd2a;"></div>
+				<div class="skill-bar-percent">85%</div>
+			</div>
+			<!-- /PHP  -->
+			<!-- Python  -->
+			<div class="skillbar clearfix " data-percent="10%">
+				<div class="skillbar-title" style="background: #EB6260;"><span>Python</span></div>
+				<div class="skillbar-bar" style="background: #EB6260;"></div>
+				<div class="center">loading...</div>
+			</div>
+			<!-- /Python  -->
+			<!-- Swift3  -->
+			<div class="skillbar clearfix " data-percent="20%">
+				<div class="skillbar-title" style="background: #EB6260;"><span>Swift3</span></div>
+				<div class="skillbar-bar" style="background: #EB6260;"></div>
+				<div class="center">loading...</div>
+			</div>
+			<!-- /Swift3  -->
+			<p>Databases :</p>
+			<!-- mysql  -->
+			<div class="skillbar clearfix " data-percent="80%">
+				<div class="skillbar-title" style="background: #88cd2a;"><span>MYSQL</span></div>
+				<div class="skillbar-bar" style="background: #88cd2a;"></div>
+				<div class="skill-bar-percent">80%</div>
+			</div>
+			<!-- /mysql  -->
+			<p>Version Control :</p>
+			<!--  Git -->
+			<div class="skillbar clearfix " data-percent="65%">
+				<div class="skillbar-title" style="background: #00a8ff;"><span>Git</span></div>
+				<div class="skillbar-bar" style="background: #00a8ff;"></div>
+				<div class="skill-bar-percent">65%</div>
+			</div>
+			<!-- /Git -->
+			<p>Servers :</p>
+			<!-- Apache  -->
+			<div class="skillbar clearfix " data-percent="80%">
+				<div class="skillbar-title" style="background: #88cd2a;"><span>Apache</span></div>
+				<div class="skillbar-bar" style="background: #88cd2a;"></div>
+				<div class="skill-bar-percent">80%</div>
+			</div>
+			<!-- /Apache  -->
+			<p>Frameworks :</p>
+			<!--  bootstrap -->
+			<div class="skillbar clearfix " data-percent="75%">
+				<div class="skillbar-title" style="background: #00a8ff;"><span>BootStrap</span></div>
+				<div class="skillbar-bar" style="background: #00a8ff;"></div>
+				<div class="skill-bar-percent">75%</div>
+			</div>
+			<!-- /bootstrap -->
+			<!--  Materialize -->
+			<div class="skillbar clearfix " data-percent="65%">
+				<div class="skillbar-title" style="background: #00a8ff;"><span>Materialize</span></div>
+				<div class="skillbar-bar" style="background: #00a8ff;"></div>
+				<div class="skill-bar-percent">65%</div>
+			</div>
+			<!-- /Materialize -->
+			<!--  CodeIgniter -->
+			<div class="skillbar clearfix " data-percent="85%">
+				<div class="skillbar-title" style="background: #00a8ff;"><span>CodeIgniter</span></div>
+				<div class="skillbar-bar" style="background: #00a8ff;"></div>
+				<div class="skill-bar-percent">85%</div>
+			</div>
+			<!-- /CodeIgniter -->
+			<p>Methodology :<br> OOP - Object Oriented Programming</p>
+	</div>
+</section>
+<!-- /skills -->
+<!--PRACTICAL EXPERIENCE-->
+<section>
+	<div class="experience text-center">
+		<h1>EXPERIENCE</h1>
+		<h5>MY PRACTICAL EXPERIENCE</h5>
+		<hr class="line">
+			<div>
+				<p><b>- Restart Network</b> | Rotterdam, The Netherlands (9/2016 – present)</p>
+			</div>
+			<div>
+				<p>Full-Stack Web Development Bootcamp & Teaching Assistant <br>
+				Completed an intensive 3 month web development bootcamp in December, working more than 1000 hours and graduating successfully. After graduating, I started as a Teaching Assistant and my responsibilities included assisting new students through hosting algorithm classes, live coding as well as pair programming sessions.
+				</p>
+				<p>Projects:<br>
+				* Funider (bit.ly/FuniderApp) – Developed web application with social network features such as live chat, posting activities and stories in order to organize leisure sports activities in groups. With the help of a senior cybersecurity professional I also did a penetration test to identify the application’s basic security vulnerabilities, afterwards I implemented more secure lines of code to fix the issues. Technologies used include: SQL, HTML5, CSS3, Javascript. PHP, jQuery, Bootstrap, Google Maps API, AJAX<br>
+				* Anas Alrez – Developed my own portfolio website. Technologies used include: SQL, HTML5, CSS3, Javascript, PHP, jQuery, Bootstrap
+					
+				</p>
+			</div>
+			<div>
+				<p><b>- Prolites</b> | Beirut, Lebanon (11/2011 – 12/2013)</p>
+			</div>
+			<div>
+				<p>Sound, Light & Video Installations<br>
+				I worked to install lighting and sound effects for nightclubs and theaters.</p>
+			</div>
+	</div>
+</section>
+<!--/PRACTICAL EXPERIENCE-->
+<!-- education -->
+<section>
+	<div class="education text-center">
+		<h1>EDUCATION</h1>
+		<h5>MY EDUCATION</h5>
+		<hr class="line">
+		<div>
+			<p>
+				- Damascus University | Electronics and Communication, Damascus, SY (9/2010– 6/2011)<br>
+				Before leaving my country I studied 1 semester of a BSc degree, courses included:<br>
+				* Mechanical Engineering, Mathematics, Physics and Chemistry, Technical Drawing
+			</p>
+		</div>
+		<div>
+			<p>
+				- MTD High School | STEM Track, Damascus, SY (9/2008 – 6/2010)<br>
+				Finished my A-levels equivalent in a STEM track in Damascus, subjects included:<br>
+				* Mathematics, Physics, Chemistry, English, Arabic, Sciences, French, History
+
+			</p>
+		</div>
+	</div>
+</section>
+<!-- /education -->
+<!-- comment -->
+<?php 
+if (isset($_POST['submit'])) {
+	$to="mhd.anas.alrz@hotmail.com";
+	$subject="from your website (Form submission)";
+	$email=$_POST['email'];
+	$comment=$_POST['comment'] ."\n";
+
+		//Connect to the database
+		//$connection = mysqli_connect('localhost' , 'root' , 'root' , 'mycv');
+		//if(!$connection){
+		  //  die ("database connection is faild");
+		//}else{}
+
+		// if the email or comment input is empty
+		if ($email && $comment){}else {
+ 	    $error='"this field cannot be blank"';
+		}
+
+	// if there is no @ in the email input	
+	if($email){
+		if (!strpos($email,'@')) {
+			$EmailError='"invalid email"';
+		}
+		else {
+			// if the comment has qutation (')
+			if (strpos($comment ,chr(39))) {
+        	$comment = str_replace(chr(39), "''", $comment);
+        //start inserting data
+        //$query = "insert into comments (email,comment) values ('$email' ,'$comment')";
+        //$result=mysqli_query($connection , $query);
+            //if(!$result){
+              //  die ("query failded" . mysqli_errno());
+            //}else{
+            	$thanks="THINK YOU";
+            	$headers = "from:".$email."\r\n";
+            	mail($to,$subject,$comment,$headers);
+            //}
+        }else{
+        	//$query = "insert into comments (email,comment) values ('$email' ,'$comment')";
+        //$result=mysqli_query($connection , $query);
+          //  if(!$result){
+            //    die ("query failded" . mysqli_errno());
+            //}else{
+            	$thanks="THANK YOU";
+            	$headers = "from:".$email ."\r\n";
+            	mail($to,$subject,$comment,$headers);
+            //}
+        }
+		}
+	}
+}
+
+?>
+<form action=" " method="post">
+<div class="comment">
+	<h1 class="text-center">COMMENT</h1>
+	<h5 class="text-center">SEND YOUR COMMENT</h5>
+	<hr class="line">
+    <input type="text" name="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+    <span class="is_error"><?php if(isset($EmailError)){ echo $EmailError; }?></span><br>
+  	<textarea class="form-control" name="comment" rows="3" placeholder="Comment"></textarea>
+    <span class="is_error"><?php if(isset($error)){ echo $error; }?></span><br>
+    <span class="is_error"><?php if(isset($thanks)){ echo $thanks; }?></span><br>
+	</div>
+	<div class="comment">
+	<button type="submit" name="submit" class="btn btn-info"><span class="glyphicon glyphicon-send" aria-hidden="true"> </span> Submit</button>
+</div>
+<!-- comment ends -->
+
+<div class="copyright">
+	© Copyright <a href="http://anas.alrez.net"> M.A.Alrz </a> 2017. 
+</div>
+</form>
+
+
+  <script type="text/javascript">
+$( window ).scroll(function() {
+	if($( window ).scrollTop() > 900 ){  // scroll down abit and get the action   
+		jQuery('.skillbar').each(function(){
+			jQuery(this).find('.skillbar-bar').animate({
+				width:jQuery(this).attr('data-percent')
+			},2000);
+		});    
+	}
+	
+});
+
+
+  </script>
+  <script type="text/javascript">
+  	$(document).ready(function(){
+	$('.carousel').carousel({
+		interval:3000
+	});
+	
+});
+
+  </script>
+</body>
+</html>
